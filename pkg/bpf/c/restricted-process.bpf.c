c917e3507a83ffac885c4e7001c2e2fe8e97834d
tongyuxiang <tong63377@163.com>
2023-08-25 09:49:24 +0000

    add process record map

--- pkg/bpf/c/restricted-process.bpf.c
+++ pkg/bpf/c/restricted-process.bpf.c
@@ -4,10 +4,11 @@
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
 #include <linux/errno.h>
-// #include <linux/bpf.h>
-// #include <linux/sched.h>
 
-struct process_create_event_t {
+#define BPFCON_MAX_PROCESSES 10240
+
+
+struct process_info {
     __u32 pid;
     __u32 ppid;
     char nodename[NEW_UTS_LEN + 1];
@@ -21,7 +22,9 @@ struct {
 	__uint(value_size, sizeof(u32));
 } process_events SEC(".maps");
 
-static inline void get_process_info(struct process_create_event_t *event){
+BPF_HASH(processes, u32, struct process_info, BPFCON_MAX_PROCESSES);
+
+static inline void get_process_info(struct process_info *info){
     struct task_struct *current_task;
     struct uts_namespace *uts_ns;
     struct nsproxy *nsproxy;
@@ -29,42 +32,47 @@ static inline void get_process_info(struct process_create_event_t *event){
 
     current_task = (struct task_struct *)bpf_get_current_task();
 
-    event->pid = (u32)(bpf_get_current_pid_tgid() >> 32);
+    info->pid = (u32)(bpf_get_current_pid_tgid() >> 32);
     
-    event->ppid = (u32)(BPF_CORE_READ(current_task, real_parent, tgid));
+    info->ppid = (u32)(BPF_CORE_READ(current_task, real_parent, tgid));
     BPF_CORE_READ_INTO(&nsproxy, current_task, nsproxy);
     BPF_CORE_READ_INTO(&uts_ns, nsproxy, uts_ns);
-    BPF_CORE_READ_INTO(&event->nodename, uts_ns, name.nodename);
+    BPF_CORE_READ_INTO(&info->nodename, uts_ns, name.nodename);
     
-    bpf_get_current_comm(&event->comm, sizeof(event->comm));
+    bpf_get_current_comm(&info->comm, sizeof(info->comm));
     struct task_struct *parent_task = BPF_CORE_READ(current_task, real_parent);
-    bpf_probe_read_kernel_str(&event->parent_comm, sizeof(event->parent_comm), &parent_task->comm);
+    bpf_probe_read_kernel_str(&info->parent_comm, sizeof(info->parent_comm), &parent_task->comm);
 }
  
+static __always_inline struct process_info *
+add_process_to_map(struct process_info *info)
+{
+    bpf_map_update_elem(&processes, &info->pid, info, BPF_NOEXIST);
+    struct process_info *process = bpf_map_lookup_elem(&processes, &info->pid);
+    if (!process)
+        return NULL;
+    
+    bpf_printk("process info: %d; %d; %s", process->pid, process->ppid, process->comm);
 
+    return process;
+}
 
 SEC("tracepoint/sched/sched_process_fork")
-int BPF_PROG(restricted_process_fork) {
-    struct process_create_event_t event = {};
-    get_process_info(&event);
-    bpf_perf_event_output(ctx, &process_events, BPF_F_CURRENT_CPU, &event, sizeof(event));
+int BPF_PROG(restricted_process_fork, struct bpf_raw_tracepoint_args *args) {
+    struct process_info info = {};
+    get_process_info(&info);
+    add_process_to_map(&info);
+    bpf_perf_event_output(ctx, &process_events, BPF_F_CURRENT_CPU, &info, sizeof(info));
     return 0;
 }
 
-// SEC("tracepoint/sched/sched_process_exit")
-// int BPF_PROG(restricted_process_exit) {
+SEC("tracepoint/sched/sched_process_exit")
+int BPF_PROG(restricted_process_exit, struct bpf_raw_tracepoint_args *args) {
 
-//     struct process_exit_event_t event = {};
-    
-//     event.pid = bpf_get_current_pid_tgid() >> 32;
-//     event.ppid = bpf_get_current_pid_tgid();
-//     bpf_get_current_comm(&event.comm, sizeof(event.comm));
-
-//     bpf_perf_event_output(ctx, &process_events, BPF_F_CURRENT_CPU,
-//                           &event, sizeof(event), PROCESS_EXIT_EVENT);
-
-//     return 0;
-// }
+    u32 pid = (u32)(bpf_get_current_pid_tgid() >> 32);
+    bpf_map_delete_elem(&processes, &pid);
+    return 0;
+}
 
 // SEC("lsm/task_alloc")
 // int BPF_PROG(restricted_task_alloc, struct task_struct *task, unsigned long clone_flags) {
